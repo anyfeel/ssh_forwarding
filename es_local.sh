6809b1848e63950f43ef87922951529c9fd407de
xiaojing.tang <xiaojing.tang@upai.com>
2017-05-11 09:33:50 +0800

    update

--- es_local.sh
+++ es_local.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python paramiko_forwarding.py -p 9201 -u root -r 192.168.60.17:9200 60.191.72.6:65422
+python paramiko_forwarding.py -p 9201 -u root -r 192.168.60.36:9200 60.191.72.7:65422
